b6644fcb058a34df41d0de666effd304aedf0902
arati <arati.nankar@planertria.com>
2015-05-20 17:52:34 +0530

    change the mongodb location

--- public/js/lib/printfunction.js
+++ public/js/lib/printfunction.js
@@ -6,7 +6,7 @@ function printFunction()
 	var content_innerhtml = document.getElementById("content-data").innerHTML;
 	var document_print=window.open("","",display_setting);
 	document_print.document.open();
-	document_print.document.write('<html><head><title>Print using javascript </title></head>');
+	//document_print.document.write('<html><head><title>Print using javascript </title></head>');
 	document_print.document.write('<body style="font-family:verdana; font-size:12px;" onLoad="self.print();self.close();" >');
 	document_print.document.write(content_innerhtml);
 	document_print.document.write('</body></html>');
