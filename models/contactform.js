33fba2fa2ce2eb388e5d9134370e2bfb427baa9c
arati <arati.nankar@planertria.com>
2015-05-21 16:25:13 +0530

    change the email code

--- models/contactform.js
+++ models/contactform.js
@@ -10,15 +10,6 @@ var _ = require('underscore'),
 var Contactlist = new keystone.List('Contactlist', {
 	// use nodelete to prevent people from deleting the demo admin user
 	nodelete: true,
-});
-Contactlist.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true },
-	messagesubject: { type: Types.Textarea, initial: true },
-	messagetext:{ type: Types.Textarea, initial: true },
-	isProtected: { type: Boolean, noedit: false },
-});
-Contactlist.find().where('isAdmin', true).exec(function(err, admins) {
 	new keystone.Email('enquiry-notification').send({
 	    subject: 'New Enquiry from Yoga Australia Website',
 	    to: 'arati.nankar@planetria.com',
@@ -26,6 +17,13 @@ Contactlist.find().where('isAdmin', true).exec(function(err, admins) {
 	    fromEmail: 'from@server.com',
 	    // other locals for the email template also go here
 	});
+});
+Contactlist.add({
+	name: { type: Types.Name, required: true, index: true },
+	email: { type: Types.Email, initial: true, required: true, index: true },
+	messagesubject: { type: Types.Textarea, initial: true },
+	messagetext:{ type: Types.Textarea, initial: true },
+	isProtected: { type: Boolean, noedit: false },
 });
 
 
